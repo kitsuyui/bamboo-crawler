18c51f9cf76daf66f4b514f8f7291676d6c8103d
kitsuyui <kitsuyui@kitsuyui.com>
2019-05-31 18:41:09 +0900

    Migrate to pipenv
    - remove requirements file
    - Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,76 +1,35 @@
 #!/usr/bin/env python
-
-import subprocess
-
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip < 10
-    from pip.req import parse_requirements
-
-try: # for pip >= 10
-    from pip._internal.download import PipSession
-except ImportError: # for pip < 10
-    from pip.download import PipSession
-
-from setuptools import find_packages
-import setuptools.command.build_py as build_py
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-def _read_requirements(filename):
-    reqs_obj = parse_requirements(filename,
-                                  session=PipSession())
-    reqs_str = [str(ir.req) for ir in reqs_obj]
-    return reqs_str
-
-
-class BuildReadMeCommand(build_py.build_py):
-
-    def run(self):
-        subprocess.run([
-            'pandoc',
-            '--from', 'markdown',
-            '--to', 'rst',
-            '--output', 'README.rst',
-            'README.md'
-        ])
-
-
-class PersistentPipSyncCommand(build_py.build_py):
-
-    def run(self):
-        for filename in ('requirements.txt', 'dev-requirements.txt'):
-            subprocess.run(['pip', 'install', '-r', filename])
-        for filename in ('requirements.in', 'dev-requirements.in'):
-            subprocess.run(['pip-compile', filename])
-        subprocess.run([
-            'pip-sync',
-            'dev-requirements.txt',
-            'requirements.txt',
-        ])
-
-
-# https://docs.python.org/3.6/distutils/setupscript.html#additional-meta-data
 setup(
     name='bamboo-crawler',
     version='0.1.0',
     description='Hobby Crawler (yet)',
-    long_description='''\
-bamboo-crawler
-============
-
-bamboo-crawler enables continuous growth in crawling task like a bamboo forest.
-
-.. note:: This package is still roughly developing stage.
-   So backward compatibility is very fragile at current.
-    ''',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Yui Kitsu',
     author_email='kitsuyui+github@kitsuyui.com',
     url='https://github.com/kitsuyui/bamboo-crawler',
-    packages=find_packages(),
-    install_requires=_read_requirements('requirements.txt'),
+    packages=find_packages('bamboo_crawler.*'),
+    install_requires=[
+        'boto3',
+        'cssselect',
+        'lxml',
+        'requests',
+        'PyYAML',
+        'SQLAlchemy',
+    ],
     extras_require={
-        'dev': _read_requirements('dev-requirements.txt'),
+        'dev': [
+            'flake8',
+            'nose',
+            'pyformat',
+            'moto[server]',
+            'flake8',
+            'mypy',
+            'httpbin',
+        ],
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -84,9 +43,5 @@ bamboo-crawler enables continuous growth in crawling task like a bamboo forest.
     entry_points={'console_scripts': [
         'bamboo = bamboo_crawler.cli:main',
     ]},
-    cmdclass={
-        'dev_build_readme': BuildReadMeCommand,
-        'dev_persistent_pip_sync': PersistentPipSyncCommand,
-    },
     license=open('./LICENSE', 'r').read(),
 )
