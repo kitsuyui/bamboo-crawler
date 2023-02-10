4ddf998c1350d35822ae4211cec94dc8062ca423
kitsuyui <kitsuyui@kitsuyui.com>
2023-02-10 13:09:37 +0900

    typing work-around for old python version.

--- bamboo_crawler/interfaces/processor.py
+++ bamboo_crawler/interfaces/processor.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
-from typing import Any, Iterable, Protocol, TypeVar
+import sys
+from typing import Any, Iterable, TypeVar
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Protocol
+else:
+    from typing import Protocol
 
 from .context import Context
 
